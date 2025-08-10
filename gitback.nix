906073234bd61808c276d4e684f569522795460e
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2025-08-10 09:45:01 +0900

    declarative mountPoint

--- gitback.nix
+++ gitback.nix
@@ -84,128 +84,133 @@ in
         #!${pkgs.nushell}/bin/nu --stdin
         ${builtins.readFile ./scripts/git-credential-helper.nu}
       '';
-      genConfig = name: value: {
-        systemd.tmpfiles.rules = [
-          "d '${value.gitPath}' 0700 ${value.user} ${value.group} - -"
-        ];
-
-        fileSystems."${value.gitPath}/data" = {
-          device = "${value.targetPath}";
-          fsType = "none";
-          options = [
-            "bind"
-            "ro"
+      genConfig =
+        name: value:
+        let
+          mountPoint = "${value.gitPath}/data";
+        in
+        {
+          systemd.tmpfiles.rules = [
+            "d '${value.gitPath}' 0700 ${value.user} ${value.group} - -"
           ];
-        };
 
-        systemd.services."gitback-${name}" = {
-          description = "GitBack Backup Service for ${name}";
-          wantedBy = [ "multi-user.target" ];
-          wants = lib.optional (value.gitConfig.remotes != [ ]) "network-online.target";
-          after = lib.optional (value.gitConfig.remotes != [ ]) "network-online.target";
-          serviceConfig = {
-            Type = "oneshot";
-            ExecStart =
-              let
-                gitArgs =
-                  [
-                    "-c"
-                    "user.name=${value.gitConfig.userName}"
-                    "-c"
-                    "user.email=${value.gitConfig.userEmail}"
-                  ]
-                  ++ lib.optionals (value.gitConfig.credentialFile != null) [
-                    "-c"
-                    "credential.helper=${gitCredentialHelper} ${value.gitConfig.credentialFile}"
-                  ];
-                git = pkgs.writeScriptBin "git" ''
-                  #!${pkgs.nushell}/bin/nu
-                  def --wrapped main [...args] {
-                    ${pkgs.git}/bin/git ${lib.concatMapStringsSep " " (e: "r##'${e}'##") gitArgs} ...$args
-                  }
-                '';
-                initScript = pkgs.writeScript "gitback-init-${name}" ''
-                  #!${pkgs.nushell}/bin/nu
-                  let name = r##'${name}'##
-                  let value = r##'${builtins.toJSON value}'## | from json
+          fileSystems.${mountPoint} = {
+            device = "${value.targetPath}";
+            fsType = "none";
+            options = [
+              "bind"
+              "ro"
+            ];
+          };
 
-                  cd $value.gitPath
-                  ${git}/bin/git init -b main # TODO: consider getting branch name from config or generating it to avoid conflicts
-                  # TODO: more initialization steps if needed (e.g. encrypting the git repository with git-crypt)
-                '';
-                backupScript = pkgs.writeScript "gitback-backup-${name}" ''
-                  #!${pkgs.nushell}/bin/nu
-                  let name = r##'${name}'##
-                  let value = r##'${builtins.toJSON value}'## | from json
+          systemd.services."gitback-${name}" = {
+            description = "GitBack Backup Service for ${name}";
+            wantedBy = [ "multi-user.target" ];
+            wants = lib.optional (value.gitConfig.remotes != [ ]) "network-online.target";
+            after = lib.optional (value.gitConfig.remotes != [ ]) "network-online.target";
+            serviceConfig = {
+              Type = "oneshot";
+              ExecStart =
+                let
+                  gitArgs =
+                    [
+                      "-c"
+                      "user.name=${value.gitConfig.userName}"
+                      "-c"
+                      "user.email=${value.gitConfig.userEmail}"
+                    ]
+                    ++ lib.optionals (value.gitConfig.credentialFile != null) [
+                      "-c"
+                      "credential.helper=${gitCredentialHelper} ${value.gitConfig.credentialFile}"
+                    ];
+                  git = pkgs.writeScriptBin "git" ''
+                    #!${pkgs.nushell}/bin/nu
+                    def --wrapped main [...args] {
+                      ${pkgs.git}/bin/git ${lib.concatMapStringsSep " " (e: "r##'${e}'##") gitArgs} ...$args
+                    }
+                  '';
+                  initScript = pkgs.writeScript "gitback-init-${name}" ''
+                    #!${pkgs.nushell}/bin/nu
+                    let name = r##'${name}'##
+                    let value = r##'${builtins.toJSON value}'## | from json
 
-                  cd $value.gitPath
+                    cd $value.gitPath
+                    ${git}/bin/git init -b main # TODO: consider getting branch name from config or generating it to avoid conflicts
+                    # TODO: more initialization steps if needed (e.g. encrypting the git repository with git-crypt)
+                  '';
+                  backupScript = pkgs.writeScript "gitback-backup-${name}" ''
+                    #!${pkgs.nushell}/bin/nu
+                    let name = r##'${name}'##
+                    let value = r##'${builtins.toJSON value}'## | from json
 
-                  let should_commit = ${git}/bin/git status -s -uall ./data/ | is-not-empty
-                  if $should_commit {
-                    print $'Committing changes for ($name)'
-                    ${git}/bin/git add ./data/
-                    ${git}/bin/git commit -m $'Backup at (date now | format date %+)'
-                  } else {
-                    print $'No changes to commit for ($name)'
-                  }
+                    cd $value.gitPath
 
-                  print $'Pushing changes for ($name)'
-                  for e in ($value.gitConfig.remotes | enumerate) {
-                    let i = $e.index;
-                    let remote = $e.item;
-                    let url = $remote.url | default { 
-                      if $remote.urlFile != null { 
-                        open --raw $remote.urlFile | str trim
-                      } else {
-                        null
+                    let should_commit = ${git}/bin/git status -s -uall r##'${mountPoint}'## | is-not-empty
+                    if $should_commit {
+                      print $'Committing changes for ($name)'
+                      ${git}/bin/git add r##'${mountPoint}'##
+                      ${git}/bin/git commit -m $'Backup at (date now | format date %+)'
+                    } else {
+                      print $'No changes to commit for ($name)'
+                    }
+
+                    print $'Pushing changes for ($name)'
+                    for e in ($value.gitConfig.remotes | enumerate) {
+                      let i = $e.index;
+                      let remote = $e.item;
+                      let url = $remote.url | default { 
+                        if $remote.urlFile != null { 
+                          open --raw $remote.urlFile | str trim
+                        } else {
+                          null
+                        }
                       }
+                      if $url == null {
+                        print $'No URL provided for remote ($i), skipping...'
+                        continue
+                      }
+                      ${git}/bin/git push $url main
                     }
-                    if $url == null {
-                      print $'No URL provided for remote ($i), skipping...'
-                      continue
+                  '';
+                  mainScript = pkgs.writeScript "gitback-main-${name}" ''
+                    #!${pkgs.nushell}/bin/nu
+                    let name = r##'${name}'##
+                    let value = r##'${builtins.toJSON value}'## | from json
+
+                    cd $value.gitPath
+                    let should_init = try {
+                      ${git}/bin/git rev-parse --is-inside-work-tree o+e> /dev/null
+                      false
+                    } catch {
+                      true
                     }
-                    ${git}/bin/git push $url main
-                  }
-                '';
-                mainScript = pkgs.writeScript "gitback-main-${name}" ''
-                  #!${pkgs.nushell}/bin/nu
-                  let name = r##'${name}'##
-                  let value = r##'${builtins.toJSON value}'## | from json
+                    if $should_init {
+                      print $'Initializing git repository for ($name)'
+                      ${initScript}
+                      print $'Initialization completed for ($name)'
+                    }
 
-                  cd $value.gitPath
-                  let should_init = try {
-                    ${git}/bin/git rev-parse --is-inside-work-tree o+e> /dev/null
-                    false
-                  } catch {
-                    true
-                  }
-                  if $should_init {
-                    print $'Initializing git repository for ($name)'
-                    ${initScript}
-                    print $'Initialization completed for ($name)'
-                  }
+                    print $'Backing up ($name)'
+                    ${backupScript}
+                    print $'Backup completed for ($name)'
+                  '';
+                in
+                mainScript;
+              User = value.user;
+              Group = value.group;
+            };
+          };
 
-                  print $'Backing up ($name)'
-                  ${backupScript}
-                  print $'Backup completed for ($name)'
-                '';
-              in
-              mainScript;
-            User = value.user;
-            Group = value.group;
+          systemd.timers."gitback-${name}" = {
+            description = "GitBack Backup Timer for ${name}";
+            wantedBy = [ "timers.target" ];
+            timerConfig = {
+              OnCalendar = value.schedule;
+              Persistent = true;
+              Unit = "gitback-${name}.service";
+            };
           };
         };
-
-        systemd.timers."gitback-${name}" = {
-          description = "GitBack Backup Timer for ${name}";
-          wantedBy = [ "timers.target" ];
-          timerConfig = {
-            OnCalendar = value.schedule;
-            Persistent = true;
-            Unit = "gitback-${name}.service";
-          };
-        };
-      };
       generated = lib.concatMapAttrs genConfig cfg.backups;
     in
     lib.mkIf cfg.enable {
